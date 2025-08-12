431d7398331b22615096bc9257665d59a53b5bdb
MarcosOliveir4 <marcosassis071@gmail.com>
2025-08-12 11:30:06 -0300

    first commit

---
+++ setup-jest.ts
@@ -0,0 +1,5 @@
+import '@testing-library/angular';
+import '@testing-library/jest-dom';
+import { setupZonelessTestEnv } from 'jest-preset-angular/setup-env/zoneless';
+
+setupZonelessTestEnv();
---
+++ src/app/app.spec.ts
@@ -0,0 +1,25 @@
+import { provideZonelessChangeDetection } from '@angular/core';
+import { render } from '@testing-library/angular';
+import { App } from './app';
+
+const sut = async () => {
+  return await render(App, {
+    providers: [provideZonelessChangeDetection()],
+  });
+};
+
+describe('App', () => {
+  it('should create the app', async () => {
+    const { fixture } = await sut();
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it('should render title', async () => {
+    const { fixture } = await sut();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('h1')?.textContent).toContain(
+      'umbandario-angular',
+    );
+  });
+});
