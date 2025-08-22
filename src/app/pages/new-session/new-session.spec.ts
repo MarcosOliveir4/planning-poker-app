0b1ddd739e39346084a9eb4d1b79d8ecc0582052
Marcos Oliveira <marcosassis071@gmail.com>
2025-08-22 10:49:06 -0300

    feat: enhance new session form with data-testid attributes for improved testing (#5)
    * feat: implement new session page with form and routing, update theme options
    
    * feat: enhance new session form with data-testid attributes for improved testing

--- src/app/pages/new-session/new-session.spec.ts
+++ src/app/pages/new-session/new-session.spec.ts
@@ -1,22 +1,73 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 import { NewSession } from './new-session';
 
+const user = userEvent.setup();
+
+const sut = async () => {
+  return render(NewSession, {
+    imports: [ReactiveFormsModule],
+  });
+};
+
 describe('NewSession', () => {
-  let component: NewSession;
-  let fixture: ComponentFixture<NewSession>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [NewSession],
-    }).compileComponents();
-
-    fixture = TestBed.createComponent(NewSession);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+  it('should create', async () => {
+    await sut();
+    const newSession = screen.getByTestId('new-session');
+    expect(newSession).toBeInTheDocument();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should NOT call createSession if form is invalid', async () => {
+    const { fixture } = await sut();
+    const createSessionSpy = jest.spyOn(
+      fixture.componentInstance,
+      'createSession',
+    );
+    const createButton = screen.getByTestId('new-session-button-create');
+    expect(createButton).toBeDisabled();
+
+    await user.click(createButton);
+    expect(createSessionSpy).not.toHaveBeenCalled();
+  });
+
+  it('should call createSession when form is valid and button is clicked', async () => {
+    const { fixture } = await sut();
+    const createSessionSpy = jest.spyOn(
+      fixture.componentInstance,
+      'createSession',
+    );
+    const userNameInput = screen.getByTestId('new-session-input-username');
+    const createButton = screen.getByTestId('new-session-button-create');
+
+    await user.type(userNameInput, 'John wick');
+    expect(createButton).toBeEnabled();
+
+    await user.click(createButton);
+    expect(createSessionSpy).toHaveBeenCalled();
+    expect(createSessionSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call createSession with the correct form values', async () => {
+    const { fixture } = await sut();
+    jest.spyOn(fixture.componentInstance, 'createSession');
+    const log = jest.spyOn(console, 'log');
+    const userNameInput = screen.getByTestId(
+      'new-session-input-username',
+    ) as HTMLInputElement;
+    const nameSessionInput = screen.getByTestId(
+      'new-session-input-namesession',
+    ) as HTMLInputElement;
+    const createButton = screen.getByTestId('new-session-button-create');
+
+    const typedUserName = 'Maria Silva';
+    await user.type(userNameInput, typedUserName);
+
+    await user.click(createButton);
+
+    expect(log).toHaveBeenCalledWith({
+      nameSession: nameSessionInput.value,
+      userName: typedUserName,
+    });
   });
 });
